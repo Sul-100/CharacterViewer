d16a8b0ffafa364a3725ade6d61f0a26fce2877c
Sultan R. Sultan <Sultan.R.Sultan@qvc.com>
2019-12-17 11:43:58 -0500

    Added SDWebImage lib

--- CharacterViewer/Details/DetailsViewController.swift
+++ CharacterViewer/Details/DetailsViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-//import SDWebImage
+import SDWebImage
 
 class CharacterDetailsViewController: UIViewController {
     
@@ -26,7 +26,7 @@ class CharacterDetailsViewController: UIViewController {
         titleLabel.text = character?.text.components(separatedBy: "-").first
         characterDescription.text = character?.text.components(separatedBy: "-").last
         
-        //characterImage.sd_setImage(with: URL(string: character?.imageURLString ?? ""), completed: nil)
+        characterImage.sd_setImage(with: URL(string: character?.imageURLString ?? ""), completed: nil)
     }
     
     override func viewDidLoad() {
